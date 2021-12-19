ecfad4fa4c840a5057f2660d769d689e3b9596e6
ranie <ranie.black29@gmail.com>
2021-12-19 20:52:54 -0300

    testando

--- teste_metodo.py
+++ teste_metodo.py
@@ -18,15 +18,15 @@ i = 0
 for metodo in metodos:
     i += 1
     _, imagem_tratada = cv2.threshold(imagem_cinza, 127, 255, metodo or cv2.THRESH_OTSU)
-    cv2.imwrite(f'bdcaptcha/imagem_tratada_{i}.png', imagem_tratada)
+    cv2.imwrite(f'testesmetodo/imagem_tratada_{i}.png', imagem_tratada)
 
-# imagem = Image.open("bdcaptcha/imagem_tratada_3.png")
-# imagem = imagem.convert("P")
-# imagem2 = Image.new("P", imagem.size, 255)
-#
-# for x in range(imagem.size[1]):
-#     for y in range(imagem.size[0]):
-#         cor_pixel = imagem.getpixel((y, x))
-#         if cor_pixel < 115:
-#             imagem2.putpixel((y, x), 0)
-# imagem2.save('bdcaptcha/imagemfinal.png')
+imagem = Image.open("testesmetodo/imagem_tratada_3.png")
+imagem = imagem.convert("P")
+imagem2 = Image.new("P", imagem.size, 255)
+
+for x in range(imagem.size[1]):
+    for y in range(imagem.size[0]):
+        cor_pixel = imagem.getpixel((y, x))
+        if cor_pixel < 5:
+            imagem2.putpixel((y, x), 0)
+imagem2.save('testesmetodo/imagemfinal.png')
